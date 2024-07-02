287a80abaee16092f35dee33dd92404e0e0f0079
hassanmasood200716 <hassanmasood9876@gmail.com>
2024-07-02 21:10:52 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+console.log("THIS CALCULATOR IS MADE BY HASSAN MASOOD");
+const answer = await inquirer.prompt([
+    { message: "ENTER YOUR FIRST NUMBER", type: "number", name: "FIRSTNUMBER" },
+    { message: "ENTER YOUR THIRD NUMBER", type: "number", name: "SECONDNUMBER" },
+    {
+        message: "ONE OF THE OPERATOR TO PERFORM OPERATION",
+        type: "list",
+        name: "OPERATOR",
+        choices: ["ADDITION", "SUBRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+//conditonal statement
+if (answer.OPERATOR === "ADDITION") {
+    console.log(answer.FIRSTNUMBER + answer.SECONDNUMBER);
+}
+else if (answer.OPERATOR === "SUBRACTION") {
+    console.log(answer.FIRSTNUMBER - answer.SECONDNUMBER);
+}
+else if (answer.OPERATOR === "MULTIPLICATION") {
+    console.log(answer.FIRSTNUMBER * answer.SECONDNUMBER);
+}
+else if (answer.OPERATOR === "DIVISION") {
+    console.log(answer.FIRSTNUMBER / answer.SECONDNUMBER);
+}
+else {
+    console.log("please select valid operator");
+}
